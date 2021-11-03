ea65e2933d144e91c57f37d974c400abceb22089
SPARTACUS5329 <adityaspartacus5329@gmail.com>
2021-11-03 19:31:36 +0530

    completed proj

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Pass-Gen
-This is a password generator. It creates a hashed password for you to remember one master password only and more safety.
+
+**Problems**
+
+1. It is very hard to remember all passwords
+2. It is not safe to have the same password for every account
+3. Even if they are different, they are not very safe because they are usually related
+
+**Solutions**
+
+1. You need to remember only one master password
+2. The passwords will be dependent on the website
+3. The passwords will be pre-hashed
+
+**Setup**
+
+1. Download the zip and expand it
+2. Open `chrome://extensions` on Google Chrome
+3. Turn on Developer Mode
+4. Click on Load Unpacked and select the folder
+5. Pin the extension if you want to by clicking on the extensions icon
+
+**Usage**
+
+1. Enter a master password that will be the same for every account
+2. Enter the service i.e the website's name for which you need the password
+3. Click on Generate
+4. There will be an alert on the browser which will contain your password
+5. Copy it and use it
+
+**Extra advantage**: You don't need to store your passwords anywhere
+
+**Note**: Longer the master password, stronger the hash
---
+++ popup.js
@@ -0,0 +1,54 @@
+const hasher = (masterPassword, service) => {
+  let t = "";
+  let n = 0;
+  let temp;
+  for (let i = 0; i < service.length; i++) {
+    n += -1 * service[i].charCodeAt(0);
+  }
+
+  n = Math.abs(n);
+
+  if (n > 500) {
+    n = n / 100;
+  } else if (n > 300) {
+    n = n / 50;
+  } else {
+    n = n / 20;
+  }
+
+  let m;
+
+  for (let i = 0; i < masterPassword.length; i++) {
+    m =
+      Math.abs(masterPassword[i].charCodeAt(0)) +
+      Math.floor(-1 * i ** (i % 2) * n) +
+      1;
+    if ((i === 0 || i == masterPassword.length - 1) && m === 32) {
+      m += 5;
+    }
+    while (m < 32) {
+      m *= 2;
+    }
+    t += String.fromCharCode(m);
+  }
+
+  alert(`Your password is ${t}`);
+
+  return t;
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const masterPasswordInputField = document.getElementById("master-password");
+  const serviceInputField = document.getElementById("service");
+  const generateButton = document.getElementById("generate");
+  const generatedPasswordDiv = document.getElementById("generated-password");
+
+  masterPasswordInputField.focus();
+
+  generateButton.addEventListener("click", function () {
+    let masterPassword = masterPasswordInputField.value;
+    let service = serviceInputField.value;
+
+    hasher(masterPassword, service);
+  });
+});
